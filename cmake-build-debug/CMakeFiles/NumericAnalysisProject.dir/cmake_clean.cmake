6002cd08d629833450406f55e502d8d06b1d3ea1
Xpirr <evrenn.ispiroglu@gmail.com>
2021-06-09 15:35:17 +0300

    Version 1.

---
+++ cmake-build-debug/CMakeFiles/NumericAnalysisProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumericAnalysisProject.dir/main.c.obj"
+  "NumericAnalysisProject.exe"
+  "NumericAnalysisProject.exe.manifest"
+  "NumericAnalysisProject.pdb"
+  "libNumericAnalysisProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NumericAnalysisProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
